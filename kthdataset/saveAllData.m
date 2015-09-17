9ee6ae93e521b37775f49c905e62215feaf6f325
sia_khers <siavash.khodadadeh@gmail.com>
2015-09-17 22:59:05 +0430

    When saving data in thread fails, proper message will be printed.

--- kthdataset/saveAllData.m
+++ kthdataset/saveAllData.m
@@ -1,5 +1,17 @@
 function saveAllData(baseFeatureDataSetAddress,classes,j,dataSet,interestFrameNumbers,interestPointValues)
-    save(strcat(baseFeatureDataSetAddress,classes,'/',classes,'_',num2str(j),'.table'),'dataSet','-ascii');
-    save(strcat(baseFeatureDataSetAddress,classes,'/',classes,'_',num2str(j),'.timedata'),'interestFrameNumbers','-ascii');
-    save(strcat(baseFeatureDataSetAddress,classes,'/',classes,'_',num2str(j),'.pointvalues'),'interestPointValues','-ascii');
+    try
+        save(strcat(baseFeatureDataSetAddress,classes,'/',classes,'_',num2str(j),'.table'),'dataSet','-ascii');
+    catch
+        fprintf(strcat(baseFeatureDataSetAddress,classes,'/',classes,'_',num2str(j),'.table could not be saved.\n'))
+    end
+    try
+        save(strcat(baseFeatureDataSetAddress,classes,'/',classes,'_',num2str(j),'.timedata'),'interestFrameNumbers','-ascii');
+    catch
+        fprintf(strcat(baseFeatureDataSetAddress,classes,'/',classes,'_',num2str(j),'.timedata could not be saved.\n'))
+    end
+    try
+        save(strcat(baseFeatureDataSetAddress,classes,'/',classes,'_',num2str(j),'.pointvalues'),'interestPointValues','-ascii');
+    catch
+        fprintf(strcat(baseFeatureDataSetAddress,classes,'/',classes,'_',num2str(j),'.pointvalues could not be saved.\n'))
+    end
 end
