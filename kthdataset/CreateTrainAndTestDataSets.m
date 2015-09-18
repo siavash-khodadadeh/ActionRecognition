cc3ca0035dac1a91e24c3c4bb832c750562af368
sia_khers <siavash.khdoadadeh@gmail.com>
2015-09-18 14:54:41 +0430

    Improving code style.

--- kthdataset/CreateTrainAndTestDataSets.m
+++ kthdataset/CreateTrainAndTestDataSets.m
@@ -3,7 +3,7 @@ clear;
 clc;
 allVideoNumbers = 599;
 addpath('../baseFunctions/Base_Functions');
-baseDataSetAddress = 'dataset_3/';
+baseDataSetAddress = '../data/dataset/';
 tempclasses = dir(baseDataSetAddress);
 tempclasses(1:2) = [];
 counterClasses = 1;
@@ -11,7 +11,7 @@ numberOfDictionaryWords = 1000;
 for i = 1:length(tempclasses)
     if tempclasses(i).isdir == 1
         classes{counterClasses} = tempclasses(i);
-        counterClasses = counterClasses+1;
+        counterClasses = counterClasses + 1;
     end
 end
 
@@ -31,10 +31,10 @@ parfor i = 1:numberOfFolds
         allFiles(1:2) = [];
         trainSetNumbers = 1:length(allFiles);
         theNumberOfAllVideos = length(allFiles);
-        foldLength = floor(theNumberOfAllVideos/numberOfFolds);
-        foldNumbers = 1 + (i-1) * foldLength:i * foldLength;
-        if i <= rem(theNumberOfAllVideos,numberOfFolds)
-            foldNumbers = [foldNumbers,numberOfFolds*foldLength+i];
+        foldLength = floor(theNumberOfAllVideos / numberOfFolds);
+        foldNumbers = 1 + (i - 1) * foldLength:i * foldLength;
+        if i <= rem(theNumberOfAllVideos, numberOfFolds)
+            foldNumbers = [foldNumbers, numberOfFolds * foldLength + i];
         end
         foldNumbers = floor(foldNumbers);
         testSetNumbers = (foldNumbers);
@@ -42,17 +42,17 @@ parfor i = 1:numberOfFolds
 %         clear k;
         k = [];
         for k = 1:length(trainSetNumbers)
-            data = importdata(strcat(baseDataSetAddress,classes{j}.name,'/',allFiles(trainSetNumbers(k)).name));
+            data = importdata(strcat(baseDataSetAddress, classes{j}.name, '/', allFiles(trainSetNumbers(k)).name));
             trainSet = [trainSet;data];
-            fprintf('%.3f percent of train videos completed\nClass = %s\n',k/length(trainSetNumbers)*100,classes{j}.name);
+            fprintf('%.3f percent of train videos completed\nClass = %s\n', k / length(trainSetNumbers) * 100, classes{j}.name);
         end
         for k = 1:length(testSetNumbers)
-            data = importdata(strcat(baseDataSetAddress,classes{j}.name,'/',allFiles(testSetNumbers(k)).name));
+            data = importdata(strcat(baseDataSetAddress,classes{j}.name, '/', allFiles(testSetNumbers(k)).name));
             testSet = [testSet;data];
-            fprintf('%.3f percent of test videos completed\n',k/length(testSetNumbers)*100);
+            fprintf('%.3f percent of test videos completed\n', k / length(testSetNumbers) * 100);
         end
-        allTrainNumbers = [allTrainNumbers,trainSetNumbers+offSet];
-        allTestNumbers = [allTestNumbers,testSetNumbers+offSet];
+        allTrainNumbers = [allTrainNumbers,trainSetNumbers + offSet];
+        allTestNumbers = [allTestNumbers,testSetNumbers + offSet];
         offSet = offSet + theNumberOfAllVideos;
     end
     % Now we have the trainset and testset
@@ -63,58 +63,57 @@ parfor i = 1:numberOfFolds
     dataSet = trainSet;
     dataSet(:,[1,[1692:1695]]) = [];
     dataSet = double(dataSet);
-    fprintf('Applying principal components analysis\nThis may be time consuming\n');
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                tic;
+    fprintf('Applying principal components analysis\nThis may be time consuming\n');                                                                                                                                                                                                                                                                                                                tic;
     coeff = princomp(dataSet);
     newDataSet = dataSet * coeff;
-    newDataSet = newDataSet(:,1:100);
+    newDataSet = newDataSet(:, 1:100);
 %     clear dataSet;
     dataSet = [];
     toc;
     fprintf('Making dictionary words\n');
     tic;
-    [idx,clusters] = makeDictionaryWords(newDataSet,numberOfDictionaryWords);
-    newDataSet = [double(videoNumbers),newDataSet,double(pointValues),double(times),double(trainClasses)];
-    temp = [double(videoNumbers),idx,double(pointValues),double(times),double(trainClasses)];
+    [idx, clusters] = makeDictionaryWords(newDataSet,numberOfDictionaryWords);
+    newDataSet = [double(videoNumbers), newDataSet, double(pointValues), double(times), double(trainClasses)];
+    temp = [double(videoNumbers), idx, double(pointValues), double(times), double(trainClasses)];
     toc;
     
-    finalData = zeros(allVideoNumbers,numberOfDictionaryWords);
+    finalData = zeros(allVideoNumbers, numberOfDictionaryWords);
     
     testVideoNumbers = testSet(:,1);
     testTimes = testSet(:,1694);
     testClasses = testSet(:,1695);
-    testPointValues = testSet(:,[1692,1693]);
+    testPointValues = testSet(:, [1692,1693]);
     testDataSet = testSet;
-    testDataSet(:,[1,[1692:1695]]) = [];
+    testDataSet(:,[1, [1692:1695]]) = [];
     testDataSet = double(testDataSet);
     newTestDataSet = testDataSet * coeff;
-    newTestDataSet = newTestDataSet(:,1:100);
+    newTestDataSet = newTestDataSet(:, 1:100);
 %     clear testDataSet;
     testDataSet = [];
-    tempTest = zeros(size(newTestDataSet,1),1);
-    for q = 1:size(newTestDataSet,1)
-        tempTest(q) = findWord(newTestDataSet(q,:),clusters);
+    tempTest = zeros(size(newTestDataSet, 1), 1);
+    for q = 1:size(newTestDataSet, 1)
+        tempTest(q) = findWord(newTestDataSet(q, :), clusters);
     end
     
-    tempTest = [double(testVideoNumbers),tempTest,double(testPointValues),double(testTimes),double(testClasses)];
+    tempTest = [double(testVideoNumbers), tempTest, double(testPointValues), double(testTimes), double(testClasses)];
     temp = [temp;tempTest];
 %     clear tempTest;
      tempTest = [];
 %     counter = 0;
     for w = 1:allVideoNumbers
         for q = 1:numberOfDictionaryWords
-            finalData(w,q) = sum(min(temp(:,1)==w,temp(:,2)==q));
+            finalData(w, q) = sum(min(temp(:, 1)==w, temp(:, 2) == q));
         end
     end
     
-    finalClasses = [ones(100,1);2*ones(99,1);3*ones(100,1);4*ones(100,1);5*ones(100,1);6*ones(100,1)];
+    finalClasses = [ones(100, 1); 2 * ones(99, 1); 3 * ones(100, 1); 4 * ones(100, 1); 5 * ones(100, 1); 6 * ones(100, 1)];
     
    
-    trainData = finalData(allTrainNumbers,:);
-    trainClasses = finalClasses(allTrainNumbers,:);
-    testData = finalData(allTestNumbers,:);
-    testClasses = finalClasses(allTestNumbers,:);
-    saveFoldData(trainData,trainClasses,testData,testClasses,clusters,temp,i);
+    trainData = finalData(allTrainNumbers ,:);
+    trainClasses = finalClasses(allTrainNumbers, :);
+    testData = finalData(allTestNumbers, :);
+    testClasses = finalClasses(allTestNumbers, :);
+    saveFoldData(trainData, trainClasses, testData, testClasses, clusters, temp, i);
 %     save(strcat('fold',num2str(i),'.trainData'),'trainData');
 %     save(strcat('fold',num2str(i),'.trainClasses'),'trainClasses');
 %     save(strcat('fold',num2str(i),'.testData'),'testData');
---
+++ kthdataset/ReadTrainAndTestSetKTH.m
@@ -0,0 +1,88 @@
+% function readTrainAndTestSetKTH()
+% end
+tic;
+diary('../logs/readTrainAndTestSetKTH.log');
+diary on;
+clc;
+clear;
+delete(gcp('nocreate'));
+matlabpool open 16;
+addpath('../baseFunctions/Better_Understanding');
+addpath('../baseFunctions/Base_Functions');
+baseFileAddress ='../data/kthdataset_videos';
+baseFeatureDataSetAddress = '../data/kthdata/';
+gaussianFilterXSize = 13;
+gaussianFilterYSize = 13;
+taw = 1.5;
+omega = 4 / taw;
+class_boxing = 'boxing';
+class_handclpping = 'handclapping';
+class_handwaving = 'handwaving';
+class_jogging = 'jogging';
+class_running = 'running';
+class_walking = 'walking';
+classes = cell(1,6);
+classes{1} = class_boxing;
+classes{2} = class_handclpping;
+classes{3} = class_handwaving;
+classes{4} = class_jogging;
+classes{5} = class_running;
+classes{6} = class_walking;
+[temp classesSize] = size(classes);
+for i = 1:classesSize
+    baseClassFileAddress = strcat(baseFileAddress,'/',classes{i},'/');
+    allFileAddresses = dir(baseClassFileAddress);
+    allFileAddresses(1:2) = [];
+    parfor j = 1:length(allFileAddresses)
+%    for j = 1:4
+        [fid,err] = fopen(strcat(baseFeatureDataSetAddress,classes{i},'/',classes{i},'_',num2str(j),'.table'));
+        if ~isempty(err)
+            fileAddress = strcat(baseClassFileAddress,allFileAddresses(j).name);
+            timeFilterSize = 7;
+            localMaximumXSize = 3;
+            localMaximumYSize = 3;
+            localMaximumTimeSize = 3;
+            dataXSize = 13; %How many pixels are needed for descriptor around interest point in x direction
+            dataYSize = 13; %How many pixels are needed for descriptor around interest point in y direction
+            dataZSize = 10; %How many pixels are needed for descriptor around interest point in z direction
+            [dataSet,interestFrameNumbers,interestPointValues] = readAMovieAndApplyFilter200Features(fileAddress,gaussianFilterXSize,gaussianFilterYSize,taw,omega,timeFilterSize,localMaximumXSize,localMaximumYSize,localMaximumTimeSize,dataXSize,dataYSize,dataZSize);
+%             save(strcat(baseFeatureDataSetAddress,classes{i},'/',classes{i},'_',num2str(j),'.table'),'dataSet','-ascii');
+%             save(strcat(baseFeatureDataSetAddress,classes{i},'/',classes{i},'_',num2str(j),'.timedata'),'interestFrameNumbers','-ascii');
+%             save(strcat(baseFeatureDataSetAddress,classes{i},'/',classes{i},'_',num2str(j),'.pointvalues'),'interestPointValues','-ascii');
+            saveAllData(baseFeatureDataSetAddress, classes{i}, j, dataSet, interestFrameNumbers, interestPointValues);
+        end
+    end
+%      delete(gcp('nocreate'));
+end
+delete(gcp('nocreate'));
+diary off;
+
+% firstFileNum = 101;
+% lastFileNum = 326;
+% allFileAddresses = dir(baseFileAddress);
+% allFileAddresses(1:2) = [];
+% tic;
+% for i = firstFileNum:lastFileNum
+%     [fId,err] = fopen(strcat(baseFeatureDataSetAddress,num2str(i),'.table'));
+% %     if ~isempty(err)
+%         fileAddress = strcat(baseFileAddress,allFileAddresses(i).name);
+%         timeFilterSize = 7;
+%         localMaximumXSize = 13;
+%         localMaximumYSize = 13;
+%         localMaximumTimeSize = 11;
+%         [cubes] = readAMovieAndApplyFilter(fileAddress,gaussianFilterXSize,gaussianFilterYSize,taw,omega,timeFilterSize,localMaximumXSize,localMaximumYSize,localMaximumTimeSize);
+%         dispaly([1]);
+% %         [dataSet] = readAMoviePartByPartAndSaveDataset(fileAddress,gaussianFilterXSize,gaussianFilterYSize,taw,omega,timeFilterSize,localMaximumXSize,localMaximumYSize,localMaximumTimeSize);
+% %         [dataSet] = readAMovieAndSaveDataset(fileAddress,gaussianFilterXSize,gaussianFilterYSize,taw,omega);
+% %         save(strcat(baseFeatureDataSetAddress,num2str(i),'.table'),'dataSet');
+% %     end
+% end
+% toc;
+% % save and load
+% 
+% 
+% % numberOfDictionaryWords = 100;
+% % [IDX clusters] = kmeans(newDataSet,numberOfDictionaryWords,'distance','sqEuclidean');
+% 
+% % imshow(result(:,:,125));
+toc;
