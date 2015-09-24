cba0e2b6084a01e3d68ed78f05fc35920de72b95
sia_khers <siavash.khodadadeh@gmail.com>
2015-09-24 20:14:00 +0330

    Prepare Data for word 2 vec added.

---
+++ kthdataset/prepareDataForWord2Vec.m
@@ -0,0 +1,33 @@
+clear;
+clc;
+clusterDataAddress = '../data/all_folds/25-folds-correct/fold1.clusters';
+pcaCoeffAddress = '../data/all_folds/25-folds-correct/fold1.pcaCoeffData';
+videoBaseAddress = '../data/dataset/';
+videoClasses = {'boxing', 'handclapping', 'handwaving', 'jogging', 'running', 'walking'};
+clusterData = importdata(clusterDataAddress);
+pcaCoeff = importdata(pcaCoeffAddress);
+delete(gcp('nocreate'));
+matlabpool open 16;
+for k = 1:length(videoClasses)
+    allFiles = dir(strcat(videoBaseAddress, videoClasses{k}, '/'));
+    allFiles(1:2) = [];
+    parfor w = 1:size(allFiles, 1)
+%         fprintf('filename is %s\n', allFiles(w).name);
+        videoAddress = allFiles(w).name;
+        videoData = importdata(strcat(videoBaseAddress, videoClasses{k}, '/', videoAddress));
+        videoDataPca = double(videoData(:, 2:1691)) * pcaCoeff;
+        videoDataPca = videoDataPca(:, 1:100);
+        videoTimeData = zeros(size(videoData, 1), 2);
+        for i = 1:size(videoData, 1)
+            wordData = videoDataPca(i, :);
+            wordNumber = findWord(wordData, clusterData);
+            videoTimeData(i, :) = [wordNumber, videoData(i, 1694)];
+            if rem(i, 10) == 0
+                fprintf('video %s: %0.2f percent completed.\n', allFiles(w).name, i / size(videoData, 1) * 100)
+            end
+        end
+        videoTimeData = sortrows(videoTimeData, 2);
+        save(strcat('../data/word2vec_data/', videoClasses{k}, '/',allFiles(w).name), 'videoTimeData', '-ascii')
+    end
+end
+delete(gcp('nocreate'));
